de7344a82ba2332f1ca04c3ceccbecf3a074a737
jatanbhalodiya <jatanbhalodiyadpsv@gmail.com>
2025-09-19 10:08:27 +0530

    Add files via upload

---
+++ classcode20.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int sum = 0;
+
+    for (int i = 1; i <= 100; i++) {
+        if (i % 3 == 0) {
+            sum += i;
+        }
+    }
+
+    printf("Sum of all integers divisible by 3 from 1 to 100 is: %d\n", sum);
+
+    return 0;
+}
